be0b9c6ee4d6037a1726df163a5f696c5008c4d8
Jacob Andersen <jacob@pixelmonkey.dk>
2024-11-20 15:58:57 +0100

    Added buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Alert from "./components/alert";
+import Button from "./components/button";
 function app() {
   return (
     <div>
       <Alert>
         Hello <strong>World</strong>
       </Alert>
+      <Button onClick={() => console.log("Clicked")}>My Button</Button>
     </div>
   );
 }
