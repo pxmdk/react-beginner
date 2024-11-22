5e7269e7e0afddd3c4da84801c903b8e2417bc13
Jacob Andersen <jacob@pixelmonkey.dk>
2024-11-22 15:24:27 +0100

    Closed down small courses, and added expense tracker

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
-import Form from "./components/Form";
+import { useState } from "react";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
+import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
+
 const App = () => {
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 10, category: "Miscellaneous" },
+    { id: 2, description: "aaa", amount: 201, category: "Entertainment" },
+    { id: 3, description: "aaa", amount: 57, category: "Groceries" },
+    { id: 4, description: "aaa", amount: 8, category: "Groceries" },
+    { id: 5, description: "aaa", amount: 62, category: "Miscellaneous" },
+  ]);
+
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((e) => e.category === selectedCategory)
+    : expenses;
+
   return (
     <div>
-      <Form></Form>
+      <div className="mb-3">
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        />
+      </div>
+
+      <ExpenseList
+        expenses={visibleExpenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      ></ExpenseList>
     </div>
   );
 };
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,28 +1,37 @@
-import { FormEvent, useRef } from "react";
+import { FormEvent, useState } from "react";
+import { useForm } from "react-hook-form";
 
 const Form = () => {
-  const nameRef = useRef<HTMLInputElement>(null);
-  const ageRef = useRef<HTMLInputElement>(null);
-  const person = { name: "", age: 0 };
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    if (nameRef.current !== null) person.name = nameRef.current.value;
-    if (ageRef.current !== null) person.age = parseInt(ageRef.current.value);
-    console.log(person);
+  const {register, handleSubmit} = useForm();
+ 
+
+
   };
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit(data => console.log(data))}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input ref={nameRef} id="name" type="text" className="form-control" />
+        <input
+          {...register("name")}
+          value={person.name}
+          id="name"
+          type="text"
+          className="form-control"
+        />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input ref={ageRef} id="age" type="number" className="form-control" />
+        <input
+          {...register("age")}
+          value={person.age}
+          id="age"
+          type="number"
+          className="form-control"
+        />
       </div>
       <button className="btn btn-primary" type="submit">
         Submit
