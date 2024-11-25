7bff4dc5b341a29d50eea4fd15b4dc12135b6455
Jacob Andersen <jacob@pixelmonkey.dk>
2024-11-25 13:31:19 +0100

    Finished expense tracker task

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
 import { useState } from "react";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
 import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
+import ExpenseForm from "./expense-tracker/components/ExpenseForm";
+
+export const categories = [
+  "Groceries",
+  "Utilities",
+  "Entertainment",
+  "Miscellaneous",
+] as const;
 
 const App = () => {
   const [selectedCategory, setSelectedCategory] = useState("");
@@ -18,6 +26,9 @@ const App = () => {
 
   return (
     <div>
+      <div className="mb-5">
+        <ExpenseForm />
+      </div>
       <div className="mb-3">
         <ExpenseFilter
           onSelectCategory={(category) => setSelectedCategory(category)}
